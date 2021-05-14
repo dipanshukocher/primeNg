397e9e5ac84990b1afa824cd582e3f895b5cc14c
DIPANSHU KOCHER <dipanshukocher2796@gmail.com>
2021-05-14 22:00:36 +0530

    text cases and services structure changes

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/primeng'));
+app.use(express.static('./dist/ciena'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/primeng/'}),
+    res.sendFile('index.html', {root: 'dist/ciena/'}),
 );
 
 // Start the app by listening on the default Heroku port
